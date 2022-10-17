81c61ac7e6e5d27034d83c308e569690fe2baf18
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2022-10-17 19:36:27 +0000

    Added vim plug install

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+# Install Oh-my-Bash
 bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
+
+# Install vim-plug plugin manager
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
