09e7dd7dad7edc882aa9f35668035bfa60ecf6c9
suasuasuasuasua <justinhoang@mines.edu>
2023-01-21 18:49:10 -0700

    Added .vim folder

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # dotfiles
+
+Check out my dotfiles! I really only have `vim` stuff, but I hope to make a cool
+linux setup in the future.
+
--- install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# Install Oh-my-Bash
-bash -c "$(curl -fsSL https://raw.githubusercontent.com/ohmybash/oh-my-bash/master/tools/install.sh)"
-
-# Install vim-plug plugin manager
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
