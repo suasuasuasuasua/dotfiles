10fb88ba957a8203336b07629db56adf5dd24f80
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2024-05-15 12:50:18 -0600

    Invoke interactive shell when installing zsh vi mode

--- setup.sh
+++ setup.sh
@@ -24,7 +24,7 @@ echo '  Installing Oh My Zsh...'
 yes | sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" --unattended
 
 # zsh vi-mode
-zsh -c "git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode"
+zsh -ic "git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH_CUSTOM/plugins/zsh-vi-mode"
 
 # Install homebrew (macOS and Linux package manager)
 echo '  Installing homebrew...'
