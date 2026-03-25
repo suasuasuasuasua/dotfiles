780e3f6ef9677d61b627f817ec19b5688c39c407
Justin Hoang <justinhoang@sua.dev>
2026-03-24 21:14:55 -0600

    feat(nvim): use mini complete and snippets
    Signed-off-by: Justin Hoang <justinhoang@sua.dev>

--- nvim/.config/nvim/lua/plugins.lua
+++ nvim/.config/nvim/lua/plugins.lua
@@ -18,14 +18,6 @@ vim.pack.add {
   'https://github.com/nvim-treesitter/nvim-treesitter',
   'https://github.com/rafamadriz/friendly-snippets',
   'https://github.com/stevearc/conform.nvim',
-  {
-    src = 'https://github.com/L3MON4D3/LuaSnip',
-    version = vim.version.range '2.0.0 - 3.0.0',
-  },
-  {
-    src = 'https://github.com/Saghen/blink.cmp',
-    version = vim.version.range '1.0.0 - 2.0.0',
-  },
 }
 
 require('ufo').setup {
@@ -101,39 +93,6 @@ require('gitsigns').setup {
 }
 
 require('guess-indent').setup {}
-
-require('blink.cmp').setup {
-  keymap = {
-    -- All presets have the following mappings:
-    -- <tab>/<s-tab>: move to right/left of your snippet expansion
-    -- <c-space>: Open menu or open docs if already open
-    -- <c-n>/<c-p> or <up>/<down>: Select next/previous item
-    -- <c-e>: Hide menu
-    -- <c-k>: Toggle signature help
-    --
-    -- See :h blink-cmp-config-keymap for defining your own keymap
-    preset = 'default',
-  },
-  appearance = { nerd_font_variant = 'mono' },
-  completion = { documentation = { auto_show = false, auto_show_delay_ms = 500 } },
-  sources = { default = { 'lsp', 'path', 'snippets' } },
-  snippets = { preset = 'luasnip' },
-
-  -- Blink.cmp includes an optional, recommended rust fuzzy matcher,
-  -- which automatically downloads a prebuilt binary when enabled.
-  --
-  -- By default, we use the Lua implementation instead, but you may enable
-  -- the rust implementation via `'prefer_rust_with_warning'`
-  --
-  -- See :h blink-cmp-config-fuzzy for more information
-  fuzzy = { implementation = 'lua' },
-
-  -- Shows a signature help window while you type arguments for a function
-  signature = { enabled = true },
-}
-
-require('luasnip').setup()
-require('luasnip.loaders.from_vscode').lazy_load()
 
 require('tokyonight').setup {
   styles = {
@@ -250,6 +209,9 @@ miniclue.setup {
   },
 }
 require('mini.cmdline').setup()
+require('mini.completion').setup {
+  delay = { completion = 10, info = 10, signature = 5 },
+}
 require('mini.files').setup {
   -- prevents bug with git difftool -d with nvim.difftool
   options = { use_as_default_explorer = false, },
@@ -359,6 +321,14 @@ vim.keymap.set('n', 'yss', 'ys_', { remap = true })
 vim.keymap.set('n', '-', [[:<C-u>lua MiniFiles.open()<CR>]], { desc = 'Open parent directory' })
 
 require('mini.trailspace').setup()
+local gen_loader = require('mini.snippets').gen_loader
+require('mini.snippets').setup {
+  snippets = {
+    -- Load snippets based on current language by reading files from
+    -- "snippets/" subdirectories from 'runtimepath' directories.
+    gen_loader.from_lang(),
+  },
+}
 require('mini.statusline').setup()
 
 -- builtins
