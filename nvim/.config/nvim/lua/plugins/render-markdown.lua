7266e6da8f45546c8967a76f8d3c5cc7c141906e
Justin Hoang <justinhoang@sua.dev>
2026-03-30 17:47:35 -0600

    chore(nvim): add missing deps

--- nvim/.config/nvim/lua/plugins/render-markdown.lua
+++ nvim/.config/nvim/lua/plugins/render-markdown.lua
@@ -1,1 +1,5 @@
-vim.pack.add { 'https://github.com/MeanderingProgrammer/render-markdown.nvim' }
+vim.pack.add {
+  'https://github.com/MeanderingProgrammer/render-markdown.nvim',
+  'https://github.com/nvim-treesitter/nvim-treesitter',
+  'https://github.com/nvim-mini/mini.nvim',
+}
