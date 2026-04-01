eb279863622c44bbc983baf99a9c6021a28e3456
Justin Hoang <justinhoang@sua.dev>
2026-03-31 19:21:43 -0600

    fix(nvim): simplify without baleia plugin

--- nvim/.config/nvim/lua/plugins/neogit.lua
+++ nvim/.config/nvim/lua/plugins/neogit.lua
@@ -2,29 +2,6 @@ vim.pack.add {
   'https://github.com/NeogitOrg/neogit',
   'https://github.com/nvim-lua/plenary.nvim',
   'https://github.com/sindrets/diffview.nvim',
-  'https://github.com/m00qek/baleia.nvim',
 }
 
 vim.keymap.set('n', '<Leader>gg', '<CMD>Neogit<CR>', { desc = 'Show Neogit UI' })
-
-require('neogit').setup {
-  -- log_pager = { 'delta', '--width', '117', '--dark' },
-  log_pager = { 'delta', '--width', '117', '--dark', "--syntax-theme", "OneHalfDark" },
-}
-
-vim.g.baleia = require("baleia").setup({})
-
--- Command to colorize the current buffer
-vim.api.nvim_create_user_command("BaleiaColorize", function()
-  vim.g.baleia.once(vim.api.nvim_get_current_buf())
-end, { bang = true })
-
--- Command to show logs
-vim.api.nvim_create_user_command("BaleiaLogs", vim.cmd.messages, { bang = true })
-
-vim.api.nvim_create_autocmd({ "BufWinEnter" }, {
-  pattern = "*.log",
-  callback = function()
-    vim.g.baleia.automatically(vim.api.nvim_get_current_buf())
-  end,
-})
