c6e661f962262fa65b3f951bba47a4aa0debf705
Justin Hoang <justinhoang@sua.dev>
2026-06-07 09:02:08 -0600

    feat(nvim): hide gitignored files

--- nvim/.config/nvim/lua/plugins/oil.lua
+++ nvim/.config/nvim/lua/plugins/oil.lua
@@ -2,6 +2,51 @@ vim.pack.add { 'https://github.com/barrettruth/canola.nvim' }
 
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
+local function parse_output(proc)
+  local result = proc:wait()
+  local ret = {}
+  if result.code == 0 then
+    for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+      line = line:gsub("/$", "")
+      ret[line] = true
+    end
+  end
+  return ret
+end
+
+local function new_git_status()
+  return setmetatable({}, {
+    __index = function(self, key)
+      local ignore_proc = vim.system(
+        { "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" },
+        {
+          cwd = key,
+          text = true,
+        }
+      )
+      local tracked_proc = vim.system({ "git", "ls-tree", "HEAD", "--name-only" }, {
+        cwd = key,
+        text = true,
+      })
+      local ret = {
+        ignored = parse_output(ignore_proc),
+        tracked = parse_output(tracked_proc),
+      }
+
+      rawset(self, key, ret)
+      return ret
+    end,
+  })
+end
+local git_status = new_git_status()
+
+local refresh = require("oil.actions").refresh
+local orig_refresh = refresh.callback
+refresh.callback = function(...)
+  git_status = new_git_status()
+  orig_refresh(...)
+end
+
 local function oil_run_on_selection(opts)
   opts = opts or {}
   local oil = require("oil")
@@ -56,6 +101,20 @@ end
 
 local detail = false
 require('oil').setup {
+  view_options = {
+    is_hidden_file = function(name, bufnr)
+      local dir = require("oil").get_current_dir(bufnr)
+      local is_dotfile = vim.startswith(name, ".") and name ~= ".."
+      if not dir then
+        return is_dotfile
+      end
+      if is_dotfile then
+        return not git_status[dir].tracked[name]
+      else
+        return git_status[dir].ignored[name]
+      end
+    end,
+  },
   keymaps = {
     ['<C-l>'] = 'actions.select',
     ['<C-h>'] = { 'actions.parent', mode = 'n' },
