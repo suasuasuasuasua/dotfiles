6acdbdbe7ebcdbb8859059cbe625e8a801396952
Justin Hoang <justinhoang@sua.dev>
2026-06-12 11:13:49 -0600

    refactor: use mini.git and mini.diff

--- nvim/.config/nvim/lua/plugins/mini.lua
+++ nvim/.config/nvim/lua/plugins/mini.lua
@@ -52,6 +52,38 @@ require('mini.cmdline').setup()
 require('mini.completion').setup {
   delay = { completion = 10, info = 10, signature = 5 },
 }
+
+local diff = require 'mini.diff'
+diff.setup {
+  view = {
+    style = 'sign',
+    signs = { add = '+', change = '~', delete = '_' },
+  },
+}
+
+vim.keymap.set('n', ']c', function()
+  if vim.wo.diff then
+    vim.cmd.normal { ']c', bang = true }
+  else
+    diff.goto_hunk 'next'
+  end
+end, { desc = 'Jump to next git [c]hange' })
+vim.keymap.set('n', '[c', function()
+  if vim.wo.diff then
+    vim.cmd.normal { '[c', bang = true }
+  else
+    diff.goto_hunk 'prev'
+  end
+end, { desc = 'Jump to previous git [c]hange' })
+vim.keymap.set({ 'n', 'v' }, '<leader>hr', 'gH', { remap = true, desc = 'git [r]eset hunk' })
+vim.keymap.set('n', '<leader>hp', diff.toggle_overlay, { desc = 'git [p]review hunk (overlay)' })
+vim.keymap.set('n', '<leader>tD', diff.toggle_overlay, { desc = '[T]oggle git [D]iff overlay' })
+
+require('mini.git').setup()
+vim.keymap.set({ 'n', 'x' }, '<leader>hb', function() MiniGit.show_at_cursor() end,
+  { desc = 'git [b]lame line / show at cursor' })
+vim.keymap.set('n', '<leader>hd', '<cmd>Git diff --cached -- %<CR>', { desc = 'git [d]iff against index' })
+vim.keymap.set('n', '<leader>hD', '<cmd>Git diff HEAD -- %<CR>', { desc = 'git [D]iff against last commit' })
 local function parse_git_output(proc)
   local result = proc:wait()
   local ret = {}
--- nvim/.config/nvim/lua/plugins/neogit.lua
+++
@@ -1,7 +0,0 @@
-vim.pack.add {
-  'https://github.com/NeogitOrg/neogit',
-  'https://github.com/nvim-lua/plenary.nvim',
-  'https://github.com/sindrets/diffview.nvim',
-}
-
-vim.keymap.set('n', '<Leader>gg', '<CMD>Neogit<CR>', { desc = 'Show Neogit UI' })
