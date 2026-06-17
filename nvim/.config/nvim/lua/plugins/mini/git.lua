d6de783eaa1611ad697a11f65b46204d1d4af2ae
Justin Hoang <justinhoang@sua.dev>
2026-06-17 06:46:36 -0600

    fix: make MiniGit and MiniDiff more simple and useful
    Signed-off-by: Justin Hoang <justinhoang@sua.dev>

--- nvim/.config/nvim/lua/plugins/mini/git.lua
+++ nvim/.config/nvim/lua/plugins/mini/git.lua
@@ -6,26 +6,25 @@ diff.setup {
   },
 }
 
-vim.keymap.set('n', ']c', function()
-  if vim.wo.diff then
-    vim.cmd.normal { ']c', bang = true }
-  else
-    diff.goto_hunk 'next'
-  end
-end, { desc = 'Jump to next git [c]hange' })
-vim.keymap.set('n', '[c', function()
-  if vim.wo.diff then
-    vim.cmd.normal { '[c', bang = true }
-  else
-    diff.goto_hunk 'prev'
-  end
-end, { desc = 'Jump to previous git [c]hange' })
-vim.keymap.set({ 'n', 'v' }, '<leader>hr', 'gH', { remap = true, desc = 'git [r]eset hunk' })
 vim.keymap.set('n', '<leader>hp', diff.toggle_overlay, { desc = 'git [p]review hunk (overlay)' })
-vim.keymap.set('n', '<leader>tD', diff.toggle_overlay, { desc = '[T]oggle git [D]iff overlay' })
 
 require('mini.git').setup()
-vim.keymap.set({ 'n', 'x' }, '<leader>hb', function() MiniGit.show_at_cursor() end,
-  { desc = 'git [b]lame line / show at cursor' })
+local align_blame = function(au_data)
+  if au_data.data.git_subcommand ~= 'blame' then return end
+
+  -- Align blame output with source
+  local win_src = au_data.data.win_source
+  vim.wo.wrap = false
+  vim.fn.winrestview { topline = vim.fn.line('w0', win_src) }
+  vim.api.nvim_win_set_cursor(0, { vim.fn.line('.', win_src), 0 })
+
+  -- Bind both windows so that they scroll together
+  vim.wo[win_src].scrollbind, vim.wo.scrollbind = true, true
+end
+
+local au_opts = { pattern = 'MiniGitCommandSplit', callback = align_blame }
+vim.api.nvim_create_autocmd('User', au_opts)
+vim.keymap.set({ 'n', 'x' }, '<leader>hb', '<cmd>vert Git blame -- %<CR>', { desc = 'git [b]lame line / show at cursor' })
 vim.keymap.set('n', '<leader>hd', '<cmd>Git diff --cached -- %<CR>', { desc = 'git [d]iff against index' })
 vim.keymap.set('n', '<leader>hD', '<cmd>Git diff HEAD -- %<CR>', { desc = 'git [D]iff against last commit' })
+vim.keymap.set({ 'n', 'x' }, '<Leader>hs', '<cmd>lua MiniGit.show_at_cursor()<CR>', { desc = 'Show at cursor' })
