8d614ae1ae87eace23462ea23708a1a04068527e
Justin Hoang <justinhoang@sua.dev>
2026-06-29 06:47:00 -0600

    chore: use simpler dotfiles hider
    Signed-off-by: Justin Hoang <justinhoang@sua.dev>

--- nvim/.config/nvim/lua/plugins/mini/files.lua
+++ nvim/.config/nvim/lua/plugins/mini/files.lua
@@ -1,46 +1,26 @@
 -- Toggle dotfiles (show git tracked)
-local function parse_git_output(proc)
-  local result = proc:wait()
-  local ret = {}
-  if result.code == 0 then
-    for line in vim.gsplit(result.stdout, '\n', { plain = true, trimempty = true }) do
-      line = line:gsub('/$', '')
-      ret[line] = true
-    end
-  end
-  return ret
+local show_dotfiles = true
+local filter_show = function(fs_entry) return true end
+local filter_hide = function(fs_entry) return not vim.startswith(fs_entry.name, '.') end
+local toggle_dotfiles = function()
+  show_dotfiles = not show_dotfiles
+  local new_filter = show_dotfiles and filter_show or filter_hide
+  MiniFiles.refresh { content = { filter = new_filter } }
 end
 
-local function new_git_status()
-  return setmetatable({}, {
-    __index = function(self, key)
-      local ignore_proc = vim.system({ 'git', 'ls-files', '--ignored', '--exclude-standard', '--others', '--directory' }, { cwd = key, text = true })
-      local tracked_proc = vim.system({ 'git', 'ls-tree', 'HEAD', '--name-only' }, { cwd = key, text = true })
-      local ret = {
-        ignored = parse_git_output(ignore_proc),
-        tracked = parse_git_output(tracked_proc),
-      }
-      rawset(self, key, ret)
-      return ret
-    end,
-  })
-end
-local git_status = new_git_status()
-
-local filter_git = function(fs_entry)
-  local dir = vim.fs.dirname(fs_entry.path)
-  local is_dotfile = vim.startswith(fs_entry.name, '.') and fs_entry.name ~= '..'
-  if is_dotfile then
-    return git_status[dir].tracked[fs_entry.name] == true
-  else
-    return not git_status[dir].ignored[fs_entry.name]
-  end
-end
+vim.api.nvim_create_autocmd('User', {
+  pattern = 'MiniFilesBufferCreate',
+  callback = function(args)
+    local buf_id = args.data.buf_id
+    -- Tweak left-hand side of mapping to your liking
+    vim.keymap.set('n', 'g.', toggle_dotfiles, { buffer = buf_id })
+  end,
+})
 
 require('mini.files').setup {
   -- prevents bug with git difftool -d with nvim.difftool
   options = { use_as_default_explorer = false },
-  content = { filter = filter_git },
+  -- content = { filter = filter_git },
 }
 
 -- Create mappings to modify target window via split ~
