b4aa4e49ebe179b66665c0c7c7969a5296c1385a
Justin Hoang <justinhoang@sua.dev>
2026-07-03 15:14:14 -0600

    chore: add neogen keymaps

--- nvim/.config/nvim/lua/plugins/neogen.lua
+++ nvim/.config/nvim/lua/plugins/neogen.lua
@@ -1,3 +1,8 @@
 vim.pack.add { 'https://github.com/danymat/neogen' }
 
 require('neogen').setup()
+
+vim.keymap.set('n', '<localleader>nd', function() require('neogen').generate() end, { desc = '[N]eogen generate [D]oc' })
+vim.keymap.set('n', '<localleader>nf', function() require('neogen').generate { type = 'func' } end, { desc = '[N]eogen generate [F]unc' })
+vim.keymap.set('n', '<localleader>nc', function() require('neogen').generate { type = 'class' } end, { desc = '[N]eogen generate [C]lass' })
+vim.keymap.set('n', '<localleader>nt', function() require('neogen').generate { type = 'type' } end, { desc = '[N]eogen generate [T]ype' })
