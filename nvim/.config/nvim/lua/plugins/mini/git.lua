9eb7113fcbeb6b9a561071d0d46f96ca813dfb4c
Justin Hoang <justinhoang@sua.dev>
2026-07-03 19:59:05 -0600

    chore: drop fugitive and rely on lazygit/neogit
    learn fugitive in vim only

--- nvim/.config/nvim/lua/plugins/mini/git.lua
+++ nvim/.config/nvim/lua/plugins/mini/git.lua
@@ -9,32 +9,8 @@ diff.setup {
 vim.keymap.set('n', '<leader>hp', diff.toggle_overlay, { desc = 'git [p]review hunk (overlay)' })
 
 require('mini.git').setup()
-local align_blame = function(au_data)
-  if au_data.data.git_subcommand ~= 'blame' then return end
 
-  -- Align blame output with source
-  local win_src = au_data.data.win_source
-  vim.wo.wrap = false
-  vim.fn.winrestview { topline = vim.fn.line('w0', win_src) }
-  vim.api.nvim_win_set_cursor(0, { vim.fn.line('.', win_src), 0 })
-
-  -- Bind both windows so that they scroll together
-  vim.wo[win_src].scrollbind, vim.wo.scrollbind = true, true
-
-  -- Reset scrollbind on the source window once the blame window closes
-  local win_blame = vim.api.nvim_get_current_win()
-  vim.api.nvim_create_autocmd('WinClosed', {
-    pattern = tostring(win_blame),
-    once = true,
-    callback = function()
-      if vim.api.nvim_win_is_valid(win_src) then vim.wo[win_src].scrollbind = false end
-    end,
-  })
-end
-
-local au_opts = { pattern = 'MiniGitCommandSplit', callback = align_blame }
-vim.api.nvim_create_autocmd('User', au_opts)
-vim.keymap.set({ 'n', 'x' }, '<leader>hb', '<cmd>vert Git blame -- %<CR>', { desc = 'git [b]lame line / show at cursor' })
+vim.keymap.set('n', '<leader>hb', '<cmd>Git blame<CR>', { desc = 'git [b]lame' })
 vim.keymap.set('n', '<leader>hd', '<cmd>Git diff --cached -- %<CR>', { desc = 'git [d]iff against index' })
 vim.keymap.set('n', '<leader>hD', '<cmd>Git diff HEAD -- %<CR>', { desc = 'git [D]iff against last commit' })
 vim.keymap.set({ 'n', 'x' }, '<Leader>hs', '<cmd>lua MiniGit.show_at_cursor()<CR>', { desc = 'Show at cursor' })
