68fc180909757d300201e48c8b350e202bbf7c2b
Justin Hoang <justinhoang@sua.dev>
2026-07-12 11:08:06 -0600

    fix(nvim): bump query time on auto-dark-mode
    Signed-off-by: Justin Hoang <justinhoang@sua.dev>

--- nvim/.config/nvim/lua/plugins/auto-dark-mode.lua
+++ nvim/.config/nvim/lua/plugins/auto-dark-mode.lua
@@ -1,7 +1,7 @@
 vim.pack.add { 'https://github.com/f-person/auto-dark-mode.nvim' }
 
 require('auto-dark-mode').setup {
-  update_interval = 1000,
+  update_interval = 5000,
   set_dark_mode = function()
     vim.o.background = 'dark'
     if vim.g.colors_name then vim.cmd.colorscheme(vim.g.colors_name) end
